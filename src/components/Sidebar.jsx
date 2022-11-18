be715e9c0670a08a672aa6cd2d0a8c2d24c987c1
dmkhomen <dmkhomen@gmail.com>
2022-11-18 12:36:49 +0200

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,72 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+import { MdOutlineCancel } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import { links } from "../data/dummy";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const activeMenu = true;
+
+  const activeLink =
+    "flex items-center gap-5 p-2 rounded  text-white bg-slate-500 text-md mb-2";
+  const normalLink =
+    "flex items-center gap-5 p-2 rounded text-md text-slate-700 dark:text-slate-200 dark:hover:text-black hover:bg-light-gray mb-2";
+
+  return (
+    <div className="p-2 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto">
+      {activeMenu && (
+        <>
+          <div className="flex justify-between items-center">
+            <Link
+              to="/"
+              onClick={() => {}}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+            >
+              <img
+                src="./logo.png"
+                alt="logo"
+                className="h-8 border rounded-full"
+              />
+              <span>DashBird</span>
+            </Link>
+            <TooltipComponent content="Menu" position="BottomCenter">
+              <button
+                type="button"
+                onClick={() => {}}
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+              >
+                <MdOutlineCancel />
+              </button>
+            </TooltipComponent>
+          </div>
+          <div className="mt-10">
+            {links.map((item) => (
+              <div key={item.title}>
+                <p className="text-slate-400 p-2 mt-2 uppercase">
+                  {item.title}
+                </p>
+                {item.links.map((link) => (
+                  <NavLink
+                    to={`/${link.name}`}
+                    key={link.name}
+                    onClick={() => {}}
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
+                  >
+                    {link.icon}
+                    <span className="capitalize">{link.name}</span>
+                  </NavLink>
+                ))}
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
